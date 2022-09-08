196b68206256077b8082071ad683d41122b4c9d2
giovannyTorres <giovanny.torres2013a@gmail.com>
2022-09-07 21:04:46 -0500

    notebook de pruebas y script de la función 1 modificadas

--- función1.py
+++ función1.py
@@ -1,7 +1,6 @@
 #%%
 from scipy.spatial import distance as dist
 from imutils import perspective
-from imutils import contours
 import numpy as np
 import imutils
 import cv2
@@ -12,29 +11,35 @@ def midpoint(ptA, ptB): #calcular el punto medio
 def pill_measuring(image,time,ID):
 
     # Cargar la imagen
-    image = cv2.imread(image)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Escala de gris
-    gray = cv2.GaussianBlur(gray, (13, 13), 0) #Blur Gaussiano para filtrado de ruido y cerrado de bordes
-
-    edged = cv2.Canny(gray, 0, 255) #Detectar bordes
-    edged = cv2.dilate(edged, None, iterations=1) #dilatar
-    edged = cv2.erode(edged, None, iterations=1) #erosionar
+    image = cv2.imread(image,0)
+    #img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #ret, thresh = cv2.threshold(image, 155, 255, 0)
+    #gray = cv2.GaussianBlur(image, (5, 5), 0) #Blur Gaussiano para filtrado de ruido y cerrado de bordes
+    mask = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,19,5)
+    #edged = cv2.Canny(gray, 0, 255) #Detectar bordes
+    #edged = cv2.dilate(edged, None, iterations=1) #dilatar
+    #edged = cv2.erode(edged, None, iterations=1) #erosionar
     # find contours in the edge map
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE) #Encontrar contornos cerrados
-    cnts = imutils.grab_contours(cnts) 
-    
-    #ordenar los contornos de mayor a menor
-    (cnts, _) = contours.sort_contours(cnts)
+    cnts, __ = cv2.findContours(mask.copy(), cv2.RETR_CCOMP,    cv2.CHAIN_APPROX_SIMPLE)
+        #(cnts, _) = contours.sort_contours(cnts)
+    cnts = sorted(cnts,key=len,reverse=True)
 
+    D_lst = []
     # iterar a través de cada contorno
     for c in cnts[:]:
         # ignorar si el contorno no es sificientemente grande
-        if cv2.contourArea(c) < 100:
+        if cv2.contourArea(c) < 5000:
             continue
         # Calcular la caja del contorno
         orig = image.copy() #copia de la imagen
-        box = cv2.minAreaRect(c)  #rectangulo rotado en la dirección del objeto
+        ellipse = cv2.fitEllipse(c)
+        x = np.zeros_like(orig)
+        cv2.ellipse(x,ellipse,(255,255,255),1)
+        cont, __ = cv2.findContours(
+        x.copy(),  cv2.RETR_CCOMP,    cv2.CHAIN_APPROX_NONE)
+        #(cnts, _) = contours.sort_contours(cnts)
+        cont = sorted(cont, key=len, reverse=True)
+        box = cv2.minAreaRect(cont[0])  #rectangulo rotado en la dirección del objeto
         box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box) #puntos para dibujar el rectangulo
         box = np.array(box, dtype="int") 
         # order the points in the contour such that they appear
@@ -58,10 +63,10 @@ def pill_measuring(image,time,ID):
         (tlblX, tlblY) = midpoint(tl, bl)
         (trbrX, trbrY) = midpoint(tr, br)
         # draw the midpoints on the image
-        cv2.circle(orig, (int(tltrX), int(tltrY)), 2, (255, 0, 0), -1)
-        cv2.circle(orig, (int(blbrX), int(blbrY)), 2, (255, 0, 0), -1)
-        cv2.circle(orig, (int(tlblX), int(tlblY)), 2, (255, 0, 0), -1)
-        cv2.circle(orig, (int(trbrX), int(trbrY)), 2, (255, 0, 0), -1)
+        cv2.circle(orig, (int(tltrX), int(tltrY)), 2, (255, 10, 0), -1)
+        cv2.circle(orig, (int(blbrX), int(blbrY)), 2, (255, 10, 0), -1)
+        cv2.circle(orig, (int(tlblX), int(tlblY)), 2, (255, 10, 0), -1)
+        cv2.circle(orig, (int(trbrX), int(trbrY)), 2, (255, 10, 0), -1)
         # draw lines between the midpoints
         cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
             (255, 0, 255), 2)
@@ -70,23 +75,25 @@ def pill_measuring(image,time,ID):
         # compute the Euclidean distance between the midpoints
         dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
         dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-        # if the pixels per metric has not been initialized, then
-        # compute it as the ratio of pixels to supplied metric
-        # (in this case, inches)
-        pixelsPerMetric = dB / 0.01
-        # compute the size of the object
-        dimA = dA / pixelsPerMetric
-        dimB = dB / pixelsPerMetric
+        
+        D_lst.append(dA)
+        D_lst.append(dB)
         # draw the object sizes on the image
-        cv2.putText(orig, "{:.2f}cm".format(dimA),
+
+        cv2.putText(orig, "{:.2f}pix".format(dA),
             (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
             0.65, (255, 255, 255), 2)
-        cv2.putText(orig, "{:.2f}cm".format(dimB),
+        cv2.putText(orig, "{:.2f}pix".format(dB),
             (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
             0.65, (255, 255, 255), 2)
         # show the output image
         cv2.imshow("Image", orig)
         cv2.waitKey(0)
-    return image
+
+    D_lst.sort(reverse=True)
+    ratio1 = 100*D_lst[1]/D_lst[0]
+    ratio2 = 100*D_lst[3]/D_lst[2]
+    ratio_prom = (ratio1+ratio2)/2
+    return D_lst,ratio1,ratio2,ratio_prom
 
 # %%
